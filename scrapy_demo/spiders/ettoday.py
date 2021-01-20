e7e2770f72d68eb7b5ac133fa56408fff6ac3917
mengru <4momolmr@gmail.com>
2021-01-20 14:36:34 +0800

    add content of day 26

--- scrapy_demo/spiders/ettoday.py
+++ scrapy_demo/spiders/ettoday.py
@@ -4,7 +4,8 @@ import scrapy
 class EttodaySpider(scrapy.Spider):
     name = 'ettoday'
     allowed_domains = ['www.ettoday.net']
-    start_urls = ['http://www.ettoday.net/']
+    start_urls = ['https://www.ettoday.net/news/20201004/1824032.html']
 
     def parse(self, response):
-        pass
+        for v in response.css('p::text').extract():
+         print(v, '\n')
